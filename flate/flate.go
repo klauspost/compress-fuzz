f6be5f7b9385e340876912aa5590047e1d323624
Klaus Post <klauspost@gmail.com>
2019-06-12 21:03:01 +0200

    Print level on errors. Test Reset.

--- flate/flate.go
+++ flate/flate.go
@@ -7,6 +7,7 @@ import (
 	"bytes"
 	"github.com/klauspost/compress/flate"
 	"io/ioutil"
+	"strconv"
 )
 
 func Fuzz(data []byte) int {
@@ -20,29 +21,53 @@ func Fuzz(data []byte) int {
 		return 0
 	}
 	for level := 0; level <= 9; level++ {
+		msg := "level " + strconv.Itoa(level) + ":"
 		buf := new(bytes.Buffer)
 		fw, err := flate.NewWriter(buf, level)
 		if err != nil {
-			panic(err)
+			panic(msg + err.Error())
 		}
 		n, err := fw.Write(data1)
 		if n != len(data1) {
-			panic("short write")
+			panic(msg + "short write")
 		}
 		if err != nil {
-			panic(err)
+			panic(msg + err.Error())
 		}
 		err = fw.Close()
 		if err != nil {
-			panic(err)
+			panic(msg + err.Error())
 		}
 		fr1 := flate.NewReader(buf)
 		data2, err := ioutil.ReadAll(fr1)
 		if err != nil {
-			panic(err)
+			panic(msg + err.Error())
 		}
 		if bytes.Compare(data1, data2) != 0 {
-			panic("not equal")
+			panic(msg + "not equal")
+		}
+		// Do it again...
+		msg = "level " + strconv.Itoa(level) + " (reset):"
+		buf.Reset()
+		fw.Reset(buf)
+		n, err = fw.Write(data1)
+		if n != len(data1) {
+			panic(msg + "short write")
+		}
+		if err != nil {
+			panic(msg + err.Error())
+		}
+		err = fw.Close()
+		if err != nil {
+			panic(msg + err.Error())
+		}
+		fr1 = flate.NewReader(buf)
+		data2, err = ioutil.ReadAll(fr1)
+		if err != nil {
+			panic(msg + err.Error())
+		}
+		if bytes.Compare(data1, data2) != 0 {
+			panic(msg + "not equal")
 		}
 	}
 	return 1
