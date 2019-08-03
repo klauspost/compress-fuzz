7953e57011e4fa2fba3be5a43516a62be59f4cda
Klaus Post <klauspost@gmail.com>
2019-08-03 13:05:07 +0200

    Add 4X huff0 compression test.

--- huff0/fuzz_compress.go
+++ huff0/fuzz_compress.go
@@ -22,7 +22,27 @@ func FuzzCompress(data []byte) int {
 		panic(err)
 	}
 	if !bytes.Equal(out, data) {
-		panic("decompression mismatch")
+		panic("decompression 1x mismatch")
 	}
+
+	comp, _, err = huff0.Compress4X(data, nil)
+	if err == huff0.ErrIncompressible || err == huff0.ErrUseRLE || err == huff0.ErrTooBig {
+		return 0
+	}
+	if err != nil {
+		panic(err)
+	}
+	s, remain, err = huff0.ReadTable(comp, nil)
+	if err != nil {
+		panic(err)
+	}
+	out, err = s.Decompress4X(remain, len(data))
+	if err != nil {
+		panic(err)
+	}
+	if !bytes.Equal(out, data) {
+		panic("decompression 4x mismatch")
+	}
+
 	return 1
 }
