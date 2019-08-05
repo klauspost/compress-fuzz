25d62579dcd0edb1e3cf00d374fc1fc473d8bc03
Klaus Post <klauspost@gmail.com>
2019-08-05 15:06:26 +0200

    Add more flate corpus.

--- flate/flate.go
+++ flate/flate.go
@@ -12,18 +12,6 @@ import (
 
 func Fuzz(data []byte) int {
 	// Enable to focus on decoding.
-	if false {
-		r := bytes.NewReader(data)
-		fr := flate.NewReader(r)
-		_, err := ioutil.ReadAll(fr)
-		if _, ok := err.(flate.InternalError); ok {
-			panic(err)
-		}
-		if err != nil {
-			return 0
-		}
-	}
-	data1 := data
 	for level := -2; level <= 9; level++ {
 		msg := "level " + strconv.Itoa(level) + ":"
 		buf := new(bytes.Buffer)
@@ -31,8 +19,8 @@ func Fuzz(data []byte) int {
 		if err != nil {
 			panic(msg + err.Error())
 		}
-		n, err := fw.Write(data1)
-		if n != len(data1) {
+		n, err := fw.Write(data)
+		if n != len(data) {
 			panic(msg + "short write")
 		}
 		if err != nil {
@@ -47,15 +35,15 @@ func Fuzz(data []byte) int {
 		if err != nil {
 			panic(msg + err.Error())
 		}
-		if !bytes.Equal(data1, data2) {
+		if !bytes.Equal(data, data2) {
 			panic(msg + "not equal")
 		}
 		// Do it again...
 		msg = "level " + strconv.Itoa(level) + " (reset):"
 		buf.Reset()
 		fw.Reset(buf)
-		n, err = fw.Write(data1)
-		if n != len(data1) {
+		n, err = fw.Write(data)
+		if n != len(data) {
 			panic(msg + "short write")
 		}
 		if err != nil {
@@ -70,7 +58,7 @@ func Fuzz(data []byte) int {
 		if err != nil {
 			panic(msg + err.Error())
 		}
-		if !bytes.Equal(data1, data2) {
+		if !bytes.Equal(data, data2) {
 			panic(msg + "not equal")
 		}
 	}
--- flate/run.cmd
+++ flate/run.cmd
@@ -3,5 +3,8 @@ del /Q fuzz-build.zip
 del /Q %CORPUS%\crashers\*.*
 del /Q %CORPUS%\suppressions\*.*
 
+go version >log.txt
+go env >>log.txt
 go-fuzz-build -o=fuzz-build.zip -func=Fuzz .
-go-fuzz -v=4 -bin=fuzz-build.zip -workdir=%CORPUS% -procs=5
+REM go-fuzz -minimize=5s -v=4 -bin=fuzz-build.zip -workdir=%CORPUS% -procs=5 2>>log.txt
+go-fuzz -minimize=5s -bin=fuzz-build.zip -workdir=%CORPUS% -procs=5
