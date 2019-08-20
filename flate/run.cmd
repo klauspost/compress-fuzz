9f71520e1075ab9bf4e844aebe90d66d7ef8b96b
Klaus Post <klauspost@gmail.com>
2019-08-20 13:49:00 +0200

    Add fse decompression corpus.

--- flate/run.cmd
+++ flate/run.cmd
@@ -3,8 +3,8 @@ del /Q fuzz-build.zip
 del /Q %CORPUS%\crashers\*.*
 del /Q %CORPUS%\suppressions\*.*
 
-go version >log.txt
-go env >>log.txt
+REM go version >log.txt
+REM go env >>log.txt
 go-fuzz-build -o=fuzz-build.zip -func=Fuzz .
 REM go-fuzz -minimize=5s -v=4 -bin=fuzz-build.zip -workdir=%CORPUS% -procs=5 2>>log.txt
 go-fuzz -minimize=5s -bin=fuzz-build.zip -workdir=%CORPUS% -procs=6
