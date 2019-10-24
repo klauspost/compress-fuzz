ca351fe1cb6be83764991e662b836a8d50309e97
Klaus Post <klauspost@gmail.com>
2019-10-24 13:08:41 +0200

    Add reuse tests for huff0.

--- huff0/fuzz_compress.go
+++ huff0/fuzz_compress.go
@@ -2,11 +2,13 @@ package fuzzhuff0
 
 import (
 	"bytes"
+
 	"github.com/klauspost/compress/huff0"
 )
 
 func FuzzCompress(data []byte) int {
-	comp, _, err := huff0.Compress1X(data, nil)
+	var sc huff0.Scratch
+	comp, _, err := huff0.Compress1X(data, &sc)
 	if err == huff0.ErrIncompressible || err == huff0.ErrUseRLE || err == huff0.ErrTooBig {
 		return 0
 	}
@@ -24,13 +26,40 @@ func FuzzCompress(data []byte) int {
 	if !bytes.Equal(out, data) {
 		panic("decompression 1x mismatch")
 	}
-
-	comp, _, err = huff0.Compress4X(data, nil)
+	// Reuse as 4X
+	sc.Reuse = huff0.ReusePolicyAllow
+	comp, reUsed, err := huff0.Compress4X(data, &sc)
 	if err == huff0.ErrIncompressible || err == huff0.ErrUseRLE || err == huff0.ErrTooBig {
 		return 0
 	}
 	if err != nil {
 		panic(err)
+	}
+	remain = comp
+	if !reUsed {
+		s, remain, err = huff0.ReadTable(comp, s)
+		if err != nil {
+			panic(err)
+		}
+	}
+	out, err = s.Decompress4X(remain, len(data))
+	if err != nil {
+		panic(err)
+	}
+	if !bytes.Equal(out, data) {
+		panic("decompression 4x with reuse mismatch")
+	}
+
+	s.Reuse = huff0.ReusePolicyNone
+	comp, reUsed, err = huff0.Compress4X(data, s)
+	if err == huff0.ErrIncompressible || err == huff0.ErrUseRLE || err == huff0.ErrTooBig {
+		return 0
+	}
+	if err != nil {
+		panic(err)
+	}
+	if reUsed {
+		panic("reused when asked not to")
 	}
 	s, remain, err = huff0.ReadTable(comp, nil)
 	if err != nil {
@@ -44,5 +73,28 @@ func FuzzCompress(data []byte) int {
 		panic("decompression 4x mismatch")
 	}
 
+	// Reuse as 1X
+	s.Reuse = huff0.ReusePolicyAllow
+	comp, reUsed, err = huff0.Compress1X(data, &sc)
+	if err == huff0.ErrIncompressible || err == huff0.ErrUseRLE || err == huff0.ErrTooBig {
+		return 0
+	}
+	if err != nil {
+		panic(err)
+	}
+	remain = comp
+	if !reUsed {
+		s, remain, err = huff0.ReadTable(comp, s)
+		if err != nil {
+			panic(err)
+		}
+	}
+	out, err = s.Decompress1X(remain)
+	if err != nil {
+		panic(err)
+	}
+	if !bytes.Equal(out, data) {
+		panic("decompression 1x with reuse mismatch")
+	}
 	return 1
 }
--- s2/run-decompress.cmd
+++ s2/run-decompress.cmd
@@ -6,4 +6,7 @@ del /Q %CORPUS%\suppressions\*.*
 SET /a PROCS=%NUMBER_OF_PROCESSORS%/2
 
 go-fuzz-build -o=fuzz-build.zip -tags=noasm -func=FuzzDecompress .
-go-fuzz -bin=fuzz-build.zip -workdir=%CORPUS% -procs=%PROCS%
+
+:LOOP
+go run ../timeout.go -duration=1h go-fuzz -minimize=5s -bin=fuzz-build.zip -workdir=%CORPUS% -procs=%PROCS%
+GOTO LOOP
