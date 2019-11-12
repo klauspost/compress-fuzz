6c60ea519025a3ce6bebc6230e71e57eca505ea6
Klaus Post <klauspost@gmail.com>
2019-11-12 18:52:00 +0100

    Add stateless deflate test.

--- flate/flate.go
+++ flate/flate.go
@@ -19,6 +19,60 @@ func Fuzz(data []byte) int {
 	}
 	reader := flate.NewReader(nil)
 	readerReset := reader.(flate.Resetter)
+	{
+		level := "stateless"
+		msg := "level " + level + ":"
+		buf := new(bytes.Buffer)
+		fw := flate.NewStatelessWriter(buf)
+		n, err := fw.Write(data)
+		if n != len(data) {
+			panic(msg + "short write")
+		}
+		if err != nil {
+			panic(msg + err.Error())
+		}
+		err = fw.Close()
+		if err != nil {
+			panic(msg + err.Error())
+		}
+		err = readerReset.Reset(buf, nil)
+		if err != nil {
+			panic(msg + err.Error())
+		}
+		data2, err := ioutil.ReadAll(reader)
+		if err != nil {
+			panic(msg + err.Error())
+		}
+		if !bytes.Equal(data, data2) {
+			panic(msg + "not equal")
+		}
+		// Do it again...
+		msg = "level " + level + " (reset):"
+		buf.Reset()
+		fw = flate.NewStatelessWriter(buf)
+		n, err = fw.Write(data)
+		if n != len(data) {
+			panic(msg + "short write")
+		}
+		if err != nil {
+			panic(msg + err.Error())
+		}
+		err = fw.Close()
+		if err != nil {
+			panic(msg + err.Error())
+		}
+		err = readerReset.Reset(buf, nil)
+		if err != nil {
+			panic(msg + err.Error())
+		}
+		data2, err = ioutil.ReadAll(reader)
+		if err != nil {
+			panic(msg + err.Error())
+		}
+		if !bytes.Equal(data, data2) {
+			panic(msg + "not equal")
+		}
+	}
 	for level := -2; level <= 9; level++ {
 		msg := "level " + strconv.Itoa(level) + ":"
 		buf := new(bytes.Buffer)
