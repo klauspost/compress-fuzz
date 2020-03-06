ad0d8af2a1c5a8a9b2c3e438c1e4bb280e8b32bc
Klaus Post <klauspost@gmail.com>
2020-03-06 23:15:15 +0100

    Corpus.

--- s2/run-decompress.cmd
+++ s2/run-decompress.cmd
@@ -5,7 +5,7 @@ del /Q %CORPUS%\crashers\*.*
 del /Q %CORPUS%\suppressions\*.*
 SET /a PROCS=%NUMBER_OF_PROCESSORS%*3/4
 
-go-fuzz-build -o=fuzz-build-%CORPUS%.zip -tags=noasm -func=FuzzDecompress .
+go-fuzz-build -o=fuzz-build-%CORPUS%.zip -func=FuzzDecompress .
 
 :LOOP
 go run ../timeout.go -duration=10m go-fuzz -minimize=5s -bin=fuzz-build-%CORPUS%.zip -workdir=%CORPUS% -procs=%PROCS%
