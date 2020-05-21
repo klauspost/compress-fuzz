93c657fc92d1d2466a67d8563380bb180c558c0a
Klaus Post <klauspost@gmail.com>
2020-05-21 11:20:35 +0200

    Better zstd decoder tests

--- zstd/fuzz_decompress.go
+++ zstd/fuzz_decompress.go
@@ -10,23 +10,81 @@ import (
 )
 
 func FuzzDecompress(data []byte) int {
-	dec, err := zstd.NewReader(bytes.NewBuffer(data), zstd.WithDecoderLowmem(true), zstd.WithDecoderConcurrency(1), zstd.WithDecoderMaxMemory(10<<20))
-	if err != nil {
+	const maxDecSize = 10 << 20
+	dec, derr := zstd.NewReader(bytes.NewBuffer(data), zstd.WithDecoderLowmem(true), zstd.WithDecoderConcurrency(1), zstd.WithDecoderMaxMemory(maxDecSize))
+	if derr != nil {
 		return 0
 	}
+	readB := make([]byte, maxDecSize-1)
 	defer dec.Close()
-	_, err = io.Copy(ioutil.Discard, dec)
+	n, err := ReadMax(dec, readB)
+	switch err {
+	case zstd.ErrCRCMismatch, nil, io.EOF:
+	default:
+		return 0
+	}
+	readB = readB[:n]
+
+	// Test if DecodeAll can also decode.
+	readA, err := dec.DecodeAll(data, nil)
 	switch err {
 	case nil, zstd.ErrCRCMismatch:
 	default:
-		return 0
+		if derr != zstd.ErrCRCMismatch {
+			panic(fmt.Errorf("buffer decoder could decode to len(%d), but blob decoder returned: %v", len(readB), err))
+		}
+	}
+	if !bytes.Equal(readA, readB) {
+		if derr != zstd.ErrCRCMismatch {
+			panic(fmt.Errorf("DecodeAll (1) bytes mismatched"))
+		}
 	}
 
-	_, err = dec.DecodeAll(data, nil)
+	// Try again.
+	readA, err = dec.DecodeAll(data, readA[:0])
 	switch err {
-	case nil, zstd.ErrCRCMismatch, zstd.ErrDecoderSizeExceeded:
+	case nil, zstd.ErrCRCMismatch:
 	default:
-		panic(fmt.Errorf("buffer decoder could decode, but blob decoder returned: %v", err))
+		if derr != zstd.ErrCRCMismatch {
+			panic(fmt.Errorf("buffer decoder could decode, but blob decoder (2) returned: %v", err))
+		}
 	}
+	if !bytes.Equal(readA, readB) {
+		if derr != zstd.ErrCRCMismatch {
+			panic(fmt.Errorf("DecodeAll (2) bytes mismatched"))
+		}
+	}
+
+	// Reset and use stream.
+	err = dec.Reset(ioutil.NopCloser(bytes.NewBuffer(data)))
+	if err != nil {
+		panic(err)
+	}
+	readA = make([]byte, maxDecSize-1)
+	n, err = ReadMax(dec, readA)
+	switch err {
+	case zstd.ErrCRCMismatch, nil, io.EOF:
+	default:
+		panic(fmt.Errorf("after Reset, decoder returned error : %v", err))
+	}
+	readA = readA[:n]
+
+	if !bytes.Equal(readA, readB) {
+		panic(fmt.Errorf("dec.Reset bytes mismatched"))
+	}
+
 	return 1
+}
+
+func ReadMax(r io.Reader, buf []byte) (n int, err error) {
+	max := len(buf)
+	for n < max && err == nil {
+		var nn int
+		nn, err = r.Read(buf[n:])
+		n += nn
+	}
+	if err == io.EOF {
+		err = nil
+	}
+	return
 }
