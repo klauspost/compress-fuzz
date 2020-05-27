c9d59b7bb3ca092013176dfc03a7726665a0f9f3
Klaus Post <klauspost@gmail.com>
2020-05-27 15:22:47 +0200

    Add zstd corpus.

--- s2/run-compress.cmd
+++ s2/run-compress.cmd
@@ -3,9 +3,9 @@ SET GO111MODULE=off
 del /Q fuzz-build.zip
 del /Q %CORPUS%\crashers\*.*
 del /Q %CORPUS%\suppressions\*.*
-SET /a PROCS=%NUMBER_OF_PROCESSORS%/2
+SET /a PROCS=%NUMBER_OF_PROCESSORS%*3/4
 
-go-fuzz-build -o=fuzz-build.zip -func=FuzzCompress .
+go-fuzz-build -o=fuzz-build.zip -tags=noasm -func=FuzzCompress .
 
 :LOOP
 go run ../timeout.go -duration=5m go-fuzz -minimize=5s -bin=fuzz-build.zip -workdir=%CORPUS% -procs=%PROCS%
